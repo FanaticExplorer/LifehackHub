cdd0751c0d8d1278e938970a6fe64699a0a973f2
andrew <andgurtov@gmail.com>
2025-12-14 18:18:39 +0100

    Move AppBar to separate compontent

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,63 +1,17 @@
 <script>
-import { useTheme } from 'vuetify'
+import MainAppBar from './components/common/MainAppBar.vue'
 
 export default {
   name: 'App',
-  setup() {
-    const theme = useTheme()
-
-    const toggleTheme = () => {
-      theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light'
-    }
-
-    return {
-      toggleTheme
-    }
-  },
-  
-  methods: {
-    goToHome() {
-      this.$router.push('/')
-    },
-
-    goToRandom() {
-      const randomTime = Date.now()
-      this.$router.push('/random?t=' + randomTime)
-    }
+  components: {
+    MainAppBar
   }
 }
 </script>
 
 <template>
   <v-app>
-    <v-app-bar color="primary" elevation="2">
-      <v-app-bar-title class="d-flex align-center">
-        <v-icon class="mr-2">mdi-lightbulb-on-outline</v-icon>
-        <span class="font-weight-bold">LifehackHub</span>
-      </v-app-bar-title>
-      <v-spacer></v-spacer>
-      <v-btn 
-        icon 
-        @click="goToHome" 
-        variant="text"
-      >
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
-      <v-btn 
-        icon 
-        @click="goToRandom" 
-        variant="text"
-      >
-        <v-icon>mdi-dice-5</v-icon>
-      </v-btn>
-      <v-btn 
-        icon 
-        @click="toggleTheme" 
-        variant="text"
-      >
-        <v-icon>mdi-theme-light-dark</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <MainAppBar />
     <v-main>
       <router-view></router-view>
     </v-main>
---
+++ frontend/src/components/common/MainAppBar.vue
@@ -0,0 +1,63 @@
+<script>
+import { useTheme } from 'vuetify'
+
+export default {
+  name: 'MainAppBar',
+  setup() {
+    const theme = useTheme()
+
+    const toggleTheme = () => {
+      theme.global.name.value = theme.global.name.value === 'light' ? 'dark' : 'light'
+    }
+
+    return {
+      toggleTheme
+    }
+  },
+  
+  methods: {
+    goToHome() {
+      this.$router.push('/')
+    },
+
+    goToRandom() {
+      const randomTime = Date.now()
+      this.$router.push('/random?t=' + randomTime)
+    }
+  }
+}
+</script>
+
+<template>
+  <v-app-bar color="primary" elevation="2">
+    <v-app-bar-title class="d-flex align-center">
+      <v-icon class="mr-2">mdi-lightbulb-on-outline</v-icon>
+      <span class="font-weight-bold">LifehackHub</span>
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-btn 
+      icon 
+      @click="goToHome" 
+      variant="text"
+    >
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+    <v-btn 
+      icon 
+      @click="goToRandom" 
+      variant="text"
+    >
+      <v-icon>mdi-dice-5</v-icon>
+    </v-btn>
+    <v-btn 
+      icon 
+      @click="toggleTheme" 
+      variant="text"
+    >
+      <v-icon>mdi-theme-light-dark</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<style scoped>
+</style>
